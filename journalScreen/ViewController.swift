24a0892c4f24bb113f0cd4b3f24428ed1f0c8c3c
Sruthi Kavuri <kav.sruthi@gmail.com>
2022-07-21 10:16:36 -0700

    Initial Commit

---
+++ journalScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  journalScreen
+//
+//  Created by Sruthi Kavuri on 7/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
